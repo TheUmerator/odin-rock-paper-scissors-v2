db277811e1acb156e601ed2b0628603ff2f2193d
umer <54628763+TheUmerator@users.noreply.github.com>
2024-09-21 11:40:54 +0500

    Write pseudo for game phases

---
+++ script.js
@@ -0,0 +1,38 @@
+// console.log('and were live');
+
+// step 1
+// function that gets computer choice
+// returns rock, paper, or scissors
+// randomized
+
+// step 2
+/*
+- function that gets user choice
+-returns rock, paper or scissor
+- user selected, r,p,s
+*/
+
+/* step 3
+function that plays a round
+uses the choices functions 
+write main logic that dictates which is bigger
+
+whoever wins, return the name
+if draw, return draw
+
+*/
+
+/* step 4
+function that plays the game
+uses the round function
+
+game is of 5 rounds
+keep track of round number
+
+stop after round count is 5
+
+best of 5
+
+announce the winner
+
+*/
